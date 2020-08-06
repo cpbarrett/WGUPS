2626721ee3b5ae70943bd3b4bb6182c898b15d66
cpbarrett <cpbarrett@gmail.com>
2020-08-05 18:50:42 -0700

    added resizing capability to hash table

--- hash_table.py
+++ hash_table.py
@@ -9,12 +9,13 @@ class HashTable:
     Hashtable class definition using quadratic probing
     """
 
-    def __init__(self):
+    def __init__(self, table_size=41):
         """
         table is initialized with empty values and small initial capacity
         """
         dummy_pkg = Package(0, "", "", "EOD", 0, "")
-        self.table = [dummy_pkg] * 41
+        self.table = [dummy_pkg] * table_size
+        self.element_count = 0
 
     def hash(self, pkg_id):
         """
@@ -25,7 +26,16 @@ class HashTable:
         return hash(pkg_id) % 31
 
     def resize(self):
-        pass
+        """
+        If the table is more than 70% full, expand the table size.
+        :return:
+        """
+        size = len(self.table)
+        load_factor = self.element_count / size
+        if load_factor >= 0.7:
+            dummy_pkg = Package(0, "", "", "EOD", 0, "")
+            extension = [dummy_pkg] * size
+            self.table.extend(extension)
 
     def look_up(self, pkg_id):
         """
@@ -51,6 +61,18 @@ class HashTable:
         new_pkg = Package(pkg_id, address, zip_code, deadline, weight, special_notes)
         index = self.hash(new_pkg.pkg_id)
         self.table.insert(index, new_pkg)
+        self.element_count += 1
+        self.resize()
+
+    def update(self, package):
+        """
+        Update a package if it exists in the table
+        :param package:
+        :return:
+        """
+        if self.look_up(package.pkg_id) is not None:
+            index = self.hash(package.pkg_id)
+            self.table.insert(index, package)
 
     def remove(self, pkg_id):
         """
@@ -58,5 +80,6 @@ class HashTable:
         :param pkg_id:
         :return:
         """
-        index = self.hash(pkg_id)
-        return self.table.pop(index)
+        if self.look_up(pkg_id) is not None:
+            index = self.hash(pkg_id)
+            return self.table.pop(index)
